d4f5c6e3a24d2b15c7be588d28fed0e78612c670
Matt Boler <meb0054@auburn.edu>
2019-10-22 08:19:01 -0500

    Remove build folder

--- build/catkin_generated/installspace/darknet_wrapper-extras.cmake
+++
@@ -1,2 +0,0 @@
-list(APPEND darknet_wrapper_INCLUDE_DIRS "${darknet_wrapper_DIR}/../../../include/modules")
-list(APPEND darknet_wrapper_INCLUDE_DIRS "${darknet_wrapper_DIR}/../../../include/shared")
